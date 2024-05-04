e405e4f1bd4ab9eebc761beb54edda8d0781f592
KULINININ <aleks680026032001@gmail.com>
2024-05-04 20:29:35 +0500

    feat(api): add interface for api data

---
+++ src/types/item-data.ts
@@ -0,0 +1,13 @@
+export interface ItemData {
+  id: string
+  name: string
+  code: string | null
+  price: {
+    old_price: number | null
+    current_price: number
+  }
+  image: {
+    url: string
+  }
+  material: number
+}
