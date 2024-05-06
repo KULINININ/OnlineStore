efa628f99b5efd174629a962fbb15d1fdd336a95
KULINININ <aleks680026032001@gmail.com>
2024-05-06 15:43:32 +0500

    feat(skeletons): Add ItemCardSkeleton

---
+++ src/components/Skeletons/ItemCardSkeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="item-card bg-white p-20 mb-6 shadow transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-wait border justify-center flex items-center"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-20 h-20 animate-bounce stroke-gray-300"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+      />
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
