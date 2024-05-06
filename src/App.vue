ecdafb4751aef617f72a17abccc3dfe37af06be7
KULINININ <aleks680026032001@gmail.com>
2024-05-06 15:42:14 +0500

    feat(App): add API.loadItems

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,10 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+import { useItemsStore } from './stores/items'
+
+// Load items from API
+const itemsStore = useItemsStore()
+itemsStore.loadItems()
 </script>
