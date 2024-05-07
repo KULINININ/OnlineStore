a9c03b4f4b48ae0ac523e2d85bb400515606da7e
KULINININ <aleks680026032001@gmail.com>
2024-05-07 08:58:52 +0500

    feat(TheCatalog): use route.query for filters options

--- src/pages/TheCatalog.vue
+++ src/pages/TheCatalog.vue
@@ -5,12 +5,14 @@
         class="items-filters__selector-order"
         label="Order by:"
         :options="orderOptions"
+        :selectedOption="selectedOrder"
         @selectChange="handleOrderChange"
       />
       <BaseSelector
         class="items-filters__selector-material"
         label="Material:"
         :options="materialOptions"
+        :selectedOption="selectedMaterial"
         @selectChange="handleMaterialChange"
       />
     </div>
@@ -20,12 +22,19 @@
   </div>
 </template>
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
 import ItemsGrid from '../components/ItemsGrid/ItemsGrid.vue'
 import BaseSelector from '../components/Base/BaseSelector.vue'
 
 import { useItemsStore } from '../stores/items'
 
 const itemsStore = useItemsStore()
+const router = useRouter()
+
+let selectedMaterial = ref()
+let selectedOrder = ref()
 
 const orderOptions = [
   {
@@ -68,12 +77,34 @@ const materialOptions = [
   }
 ]
 
-const handleOrderChange = (value: String) => {
-  console.log(value)
-  itemsStore.sortItems(value)
+const handleOrderChange = (order: String) => {
+  const newQuery = { ...router.currentRoute.value.query, order: order }
+  router.replace({ query: newQuery })
+  itemsStore.sortItems(order)
 }
-const handleMaterialChange = (value: String) => {
-  console.log(value)
-  itemsStore.filterItems(value)
+const handleMaterialChange = (material: String) => {
+  const newQuery = { ...router.currentRoute.value.query, material: material }
+  router.replace({ query: newQuery })
+  itemsStore.filterItems(material)
 }
+
+const applyQueryFilters = () => {
+  const { order, material } = router.currentRoute.value.query
+
+  if (material) {
+    itemsStore.filterItems(material)
+    selectedMaterial.value = material
+  }
+  if (order) {
+    itemsStore.sortItems(order)
+    selectedOrder.value = order
+  }
+}
+
+// Load items from API
+itemsStore.loadItems().then(() => {
+  applyQueryFilters()
+})
+
+onMounted(() => {})
 </script>
