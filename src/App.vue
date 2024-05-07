7612b8ea62410f2962639319952eea9bd18b3a8e
KULINININ <aleks680026032001@gmail.com>
2024-05-07 08:55:55 +0500

    feat(items): remove loadItems logic

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,4 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
-import { useItemsStore } from './stores/items'
-
-// Load items from API
-const itemsStore = useItemsStore()
-itemsStore.loadItems()
 </script>
