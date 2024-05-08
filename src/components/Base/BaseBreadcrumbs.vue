e0689195ec23ad877d5223d101a137209334d9fe
KULINININ <aleks680026032001@gmail.com>
2024-05-08 08:06:13 +0500

    feat(BaseBreadcrumbs): create BaseBreadcrumbs

---
+++ src/components/Base/BaseBreadcrumbs.vue
@@ -0,0 +1,31 @@
+<template>
+  <RouterLink
+    v-for="(pathSegments, index) in props.path"
+    :key="pathSegments"
+    :to="{ name: pathSegments }"
+    class="catalog-breadcrumb__link"
+    :class="{
+      'catalog-breadcrumb__link--last font-bold': index === props.path.length - 1
+    }"
+    >{{ EnumRouteName[pathSegments] }}</RouterLink
+  >
+</template>
+<script setup lang="ts">
+import { EnumRouteName } from '../../enums/EnumRouteName.ts'
+
+const props = defineProps({
+  path: { type: Array }
+})
+</script>
+
+<style scoped>
+.catalog-breadcrumb__link::after {
+  content: '/';
+  margin-left: 8px;
+  margin-right: 8px;
+}
+
+.catalog-breadcrumb__link--last::after {
+  content: '';
+}
+</style>
