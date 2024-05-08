173fa64cc36b998ca89d3f32ca8909bb20e4a4e3
KULINININ <aleks680026032001@gmail.com>
2024-05-08 08:01:34 +0500

    feat(ItemCardFooter): add props and emits for action buttons

--- src/components/ItemCard/ItemCardFooter.vue
+++ src/components/ItemCard/ItemCardFooter.vue
@@ -9,29 +9,43 @@
       </div>
     </div>
     <div class="item-actions flex items-center">
-      <button class="item-actions-in-cart__button" v-if="false">
+      <button
+        class="item-actions-in-cart__button transition duration-300 hover:scale-150"
+        v-if="props.inCart"
+        @click="emit('toCart')"
+      >
         <IconCheckCircle />
       </button>
       <button
         class="item-actions-to-cart__button transition duration-300 hover:scale-150"
-        v-if="true"
+        v-if="!props.inCart"
+        @click="emit('toCart')"
       >
         <IconShoppingCart />
       </button>
-      <button class="item-actions-like__button ml-2 transition duration-300 hover:scale-150">
-        <IconHeart />
+      <button
+        class="item-actions-like__button ml-2 transition duration-300 hover:scale-150"
+        @click="emit('like')"
+      >
+        <IconHeart :active="liked" />
       </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineEmits } from 'vue'
+
 import IconCheckCircle from '../../assets/icons/IconCheckCircle.vue'
 import IconShoppingCart from '../../assets/icons/IconShoppingCart.vue'
 import IconHeart from '../../assets/icons/IconHeart.vue'
 
+const emit = defineEmits(['like', 'toCart'])
+
 const props = defineProps({
   old_price: { type: Number },
-  current_price: { type: Number }
+  current_price: { type: Number },
+  liked: { type: Boolean },
+  inCart: { type: Boolean }
 })
 </script>
