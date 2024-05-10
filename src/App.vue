857e022c2297704f4fe480527925d81ffae306cf
KULINININ <aleks680026032001@gmail.com>
2024-05-10 20:20:23 +0500

    feat(app): add options loading

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
+import { useOptionsStore } from './stores/options'
+
+const optionsStore = useOptionsStore()
+const options = ref<string[]>(JSON.parse(localStorage.getItem('options') || '{}'))
+
+if (options.value) {
+  if (options.value['paginationMode']) {
+    optionsStore.setPaginationMode(options.value['paginationMode'])
+  }
+}
 </script>
